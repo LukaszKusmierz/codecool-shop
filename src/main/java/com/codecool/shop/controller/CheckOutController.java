043924f62c8ed26abf82851ac46b2083dfe56411
nikolawierzbowska <nikolawierzbowska21@gmail.com>
2023-11-16 18:43:06 +0100

    Html elements in checkOut, payment have been created.

---
+++ src/main/java/com/codecool/shop/controller/CheckOutController.java
@@ -0,0 +1,39 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.model.Product;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@WebServlet(urlPatterns = {"/checkoutCart"})
+public class CheckOutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartDao = CartDaoMem.getInstance();
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext())
+                .buildExchange(req, resp);
+        WebContext context = new WebContext(webExchange);
+
+        Set<Product> productsInCart = new HashSet<>(cartDao.getAll());
+        context.setVariable("numberItems", cartDao.getAll().size());
+        context.setVariable("totalPrice",cartDao.getTotalPrice(cartDao.getAll()));
+        context.setVariable("ctxPath", req.getContextPath());
+        context.setVariable("cartItems", productsInCart);
+        engine.process("product/checkoutCart.html", context, resp.getWriter());
+    }
+}
