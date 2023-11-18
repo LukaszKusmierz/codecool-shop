799207677b100074f663f2e25ce294e1d92a9ccc
FenglerPiotr <piotfeng@gmail.com>
2023-11-18 06:46:29 +0100

    Add to Cart -Frontend

--- src/main/webapp/static/javascript/main.js
+++ src/main/webapp/static/javascript/main.js
@@ -1,9 +1,9 @@
 const selectElementCategory = document.getElementById('category');
-selectElementCategory.addEventListener("change",optionCategory);
+selectElementCategory.addEventListener("change", optionCategory);
 
 function fetchProductsByCategory(route) {
     try {
-        const toFetch =`http://localhost:8080/products/get${route}`
+        const toFetch = `http://localhost:8080/products/get${route}`
         console.log("Fetching from " + toFetch)
         fetch(toFetch, {
             headers: {
@@ -15,26 +15,24 @@ function fetchProductsByCategory(route) {
             .catch(error => {
                 console.log(error)
             })
-    }catch (error) {
+    } catch (error) {
         console.log(error)
     }
 }
 
 
-
-
 function optionCategory(event) {
-        const selectedOption = selectElementCategory.options[selectElementCategory.selectedIndex];
-        const categoryId = selectedOption.getAttribute('data-category-id');
-        event.preventDefault();
-        if (categoryId) {
-            fetchProductsByCategory(`?categoryId=${categoryId}`)
-        }
+    const selectedOption = selectElementCategory.options[selectElementCategory.selectedIndex];
+    const categoryId = selectedOption.getAttribute('data-category-id');
+    event.preventDefault();
+    if (categoryId) {
+        fetchProductsByCategory(`?categoryId=${categoryId}`)
+    }
 }
 
 
 function showProductsByCategory(products) {
-     const container = document.querySelector('.container');
+    const container = document.getElementById('main');
     const categoryNameContainer = document.getElementById('categoryName');
     const productContainer = document.getElementById('products');
 
@@ -44,24 +42,14 @@ function showProductsByCategory(products) {
     productContainer.innerHTML = '';
     products.forEach((prod) => {
         const cardProducts = document.createElement('div');
-        cardProducts.classList.add('col', 'col-sm-12', 'col-md-6', 'col-lg-4');
-        cardProducts.innerHTML = `
-            
-             <div class="card">
-                <img src="/static/img/product_${prod.id}.jpg">
-                <div class="card-header">
-                    <h4 class="card-title">${prod.name}</h4>
-                    <p class="card-text">${prod.description}</p>
-                </div>
-                <div class="card-body">
-                    <div class="card-text">
-                        <p class="lead">${prod.defaultPrice}  ${prod.defaultCurrency}</p>
-                    </div>
-                    <div class="card-text">
-                        <a data-product-id=${prod.id} class="btn btn-success" href="#">Add to cart</a>
-                    </div>
-                </div>
-            </div>
+        cardProducts.classList.add('product');
+        cardProducts.innerHTML = `  
+                <img src="/static/img/product_${prod.id}.jpg">                                
+                <h3 class="card-title">${prod.name}</h3>
+                <p class="card-text">${prod.description}</p>                               
+                <h5 class="lead">${prod.defaultPrice}  ${prod.defaultCurrency}</h5>
+                <button data-product-id=${prod.id}>Add to cart</button>                
+                     
         `;
         productContainer.appendChild(cardProducts);
 
